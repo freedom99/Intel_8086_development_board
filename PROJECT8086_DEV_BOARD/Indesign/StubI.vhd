49f95f39fbb484db3ad77419a18c0aefa876f5bd
mrinmoysarkar <mrinmoy.pol@gmail.com>
2017-01-29 21:44:55 +0600

    A simple motherboard using Intel 8086 as the processor

---
+++ PROJECT8086_DEV_BOARD/Indesign/StubI.vhd
@@ -0,0 +1,4 @@
+-- Generated PORTMAP Stub File: Created by Capture FPGA Flow
+-- Matches PCB component pinout with simulation model
+-- Created Thursday, October 16, 2014 20:33:36 Bangladesh Standard Time
+
---
+++ PROJECT8086_DEV_BOARD/Indesign/sdf.cmd
@@ -0,0 +1,2 @@
+LOG_FILE = "j:\project8086_dev_board\Indesign\sdfout.log",
+SCOPE = :dut;
